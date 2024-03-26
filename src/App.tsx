eb45f12ce1f8541970426a0ec35fa1ed1ee0fc25
GillanCodes <38374481+GillanCodes@users.noreply.github.com>
2024-03-26 13:33:28 +0100

    feat: added random button (not working)

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,8 @@ function App() {
     <div className="App" style={{ backgroundImage: 'url(background.png)'}}>
       <div className="menu" style={{ backgroundImage: `url(/menu/${menu}.png)`}}>
 
+        <button className='random' style={{backgroundImage: "url(/menu/random_btn.png)"}}></button>
+
         <form className='stardew_fields'>
           <input type="text" className='field' value={props.name} onChange={(e) => setProps({...props, name: e.target.value})}/>
           <input type="text" className='field' value={props.farm} onChange={(e) => setProps({...props, farm: e.target.value})}/>
