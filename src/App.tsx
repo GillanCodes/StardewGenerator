f329d5ea2ea8cc5d5792f8dd09adc777d1d9efc8
GillanCodes <38374481+GillanCodes@users.noreply.github.com>
2024-03-26 12:09:43 +0100

    feat: setup background

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import "./Styles/index.scss";
+
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ backgroundImage: 'url(background.png)'}}>
     
     </div>
   );
