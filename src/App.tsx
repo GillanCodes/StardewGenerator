966b8342db7588b1c25bb1368b783cff8a124244
GillanCodes <38374481+GillanCodes@users.noreply.github.com>
2024-03-26 12:38:41 +0100

    feat: added default menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import "./Styles/index.scss";
 
 function App() {
+
+  const [menu, setMenu] = useState('field_empty')
+
+
   return (
     <div className="App" style={{ backgroundImage: 'url(background.png)'}}>
-    
+      <div className="menu" style={{ backgroundImage: `url(/menu/${menu}.png)`}}>
+
+      </div>
     </div>
   );
 }
