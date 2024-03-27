f3dc35fb73236430e0bc4a0d82a4b13379998479
GillanCodes <38374481+GillanCodes@users.noreply.github.com>
2024-03-27 10:58:31 +0100

    Feat: added random generation

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 
 import "./Styles/index.scss";
+import randomizer from "./randomizer.json";
+
 
 interface IProps 
 {
@@ -9,18 +11,33 @@ interface IProps
   thing: string | undefined
 }
 
+function getRdm(length:number)
+{
+  return Math.floor(Math.random() * length); 
+}
+
+
+
 function App() {
 
   const [menu, setMenu] = useState('field_empty')
 
   const [props, setProps] = useState<IProps>({name: "", farm: "", thing: ""});
-
+  
+  function randomize()
+  {
+    setProps({
+      name: randomizer.name[getRdm(randomizer.name.length)],
+      farm: randomizer.farm[getRdm(randomizer.farm.length)],
+      thing: randomizer.thing[getRdm(randomizer.thing.length)]
+    })
+  }
 
   return (
     <div className="App" style={{ backgroundImage: 'url(background.png)'}}>
       <div className="menu" style={{ backgroundImage: `url(/menu/${menu}.png)`}}>
 
-        <button className='random' style={{backgroundImage: "url(/menu/random_btn.png)"}}></button>
+        <button className='random' style={{backgroundImage: "url(/menu/random_btn.png)"}} onClick={() => randomize()}></button>
 
         <form className='stardew_fields'>
           <input type="text" className='field' value={props.name} onChange={(e) => setProps({...props, name: e.target.value})}/>
