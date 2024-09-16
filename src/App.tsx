331dbcccaf453e0317e9e4d02704c081c81d9f51
gillancodes <gillancodes@laposte.net>
2024-09-16 13:57:56 +0200

    fix: remove useless stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import "./Styles/index.scss";
 import randomizer from "./randomizer.json";
-import { isEmpty } from './Utils';
-import { log } from 'console';
-import { ToastContextProvider, useToasts } from './Toast/ToastContext';
-
 
 interface IProps 
 {
@@ -19,15 +15,11 @@ function getRdm(length:number)
   return Math.floor(Math.random() * length); 
 }
 
-
-
 function App() {
 
   const [props, setProps] = useState<IProps>({name: "", farm: "", thing: ""});
   const [gender, setGender] = useState(1);
 
-  const { pushToast } = useToasts()
- 
   function randomize()
   {
 
@@ -55,39 +47,26 @@ function App() {
 	  navigator.clipboard.writeText(element.target.value)
   }
 
-  const test = (e:any) => {
-    e.preventDefault();
-    pushToast({
-      title: "Field Copied",
-      content: "Text has been copied on your clipboard"
-    });
-  }
-
   return (
-    <ToastContextProvider>  
-      <div className="App" style={{ backgroundImage: 'url(background.png)'}}>
-        <div className="menu" style={{ backgroundImage: `url(/menu/field_not_empty.png)`}}>
+    <div className="App" style={{ backgroundImage: 'url(background.png)'}}>
+      <div className="menu" style={{ backgroundImage: `url(/menu/field_not_empty.png)`}}>
 
-          <button className='random' style={{backgroundImage: "url(/menu/random_btn.png)"}} onClick={() => randomize()}></button>
+        <button className='random' style={{backgroundImage: "url(/menu/random_btn.png)"}} onClick={() => randomize()}></button>
           
-          <div className="borders">
-            <div className={`red_border ${gender === 0 ? "" : "none"}`} id='male' onClick={() => setGender(0)}></div>
-            <div className={`red_border ${gender === 1 ? "" : "none"}`} id='female' onClick={() => setGender(1)}></div>
-          </div>
-          
-
-          <form className='stardew_fields'>
-            <button onClick={(e) => test(e)}>test</button>
-            <input type="text" className='field' value={props.name} onClick={(e:any) => copyText(e)} onAnimationEnd={(e:any) => e.target.classList.remove('animation')} />
-            <input type="text" className='field' value={props.farm} onClick={(e:any) => copyText(e)} onAnimationEnd={(e:any) => e.target.classList.remove('animation')} />
-            <input type="text" className='field' value={props.thing} onClick={(e:any) => copyText(e)} onAnimationEnd={(e:any) => e.target.classList.remove('animation')} />
-          </form>
-
+        <div className="borders">
+          <div className={`red_border ${gender === 0 ? "" : "none"}`} id='male' onClick={() => setGender(0)}></div>
+          <div className={`red_border ${gender === 1 ? "" : "none"}`} id='female' onClick={() => setGender(1)}></div>
         </div>
-
-        <a className='bmc' href="https://www.buymeacoffee.com/gillancodes" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/default-orange.png" alt="Buy Me A Coffee" height="41" width="174" /></a>
+      
+        <form className='stardew_fields'>
+          <input type="text" className='field' value={props.name} onClick={(e:any) => copyText(e)} onAnimationEnd={(e:any) => e.target.classList.remove('animation')} />
+          <input type="text" className='field' value={props.farm} onClick={(e:any) => copyText(e)} onAnimationEnd={(e:any) => e.target.classList.remove('animation')} />
+          <input type="text" className='field' value={props.thing} onClick={(e:any) => copyText(e)} onAnimationEnd={(e:any) => e.target.classList.remove('animation')} />
+        </form>
       </div>
-    </ToastContextProvider>
+
+      <a className='bmc' href="https://www.buymeacoffee.com/gillancodes" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/default-orange.png" alt="Buy Me A Coffee" height="41" width="174" /></a>
+    </div>
   );
 }
 
